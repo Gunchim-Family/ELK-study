a3717036e2f486098c76fb70b558314a12b498ae
leeen <leeen_diary@naver.com>
2024-07-22 23:38:56 +0900

    add springboot gunchim server

---
+++ gunchim-server/src/main/java/com/example/gunchimserver/GunchimServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gunchimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunchimServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunchimServerApplication.class, args);
+    }
+
+}
---
+++ gunchim-server/src/test/java/com/example/gunchimserver/GunchimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gunchimserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunchimServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
