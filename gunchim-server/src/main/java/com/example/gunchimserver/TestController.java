62364ca158c879af50f7f38594a6843741628444
leeen <leeen_diary@naver.com>
2024-08-13 00:14:05 +0900

    스프링 로그 파일 생성을 위한 로그백 파일 및 컨트롤러 생성

---
+++ gunchim-server/src/main/java/com/example/gunchimserver/TestController.java
@@ -0,0 +1,14 @@
+package com.example.gunchimserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+    @GetMapping("/test")
+    public void log() {
+        log.info("call testController");
+    }
+}
